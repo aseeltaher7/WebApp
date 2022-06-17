7bd0cddeb14aab11627936c9e101fb8137768ad4
Aseel Taher <aseeltaher7@gmail.com>
2022-06-17 16:05:32 +0300

    Add project files.

---
+++ WebApplication8/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        SqlConnection con = new SqlConnection(@"server=NITRO5\TEW_SQLEXPRESS; database=EmployeeDB; integrated security=true");
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public string Get()
+        {
+            //return new string[] { "value1", "value2" };
+            SqlDataAdapter da=new SqlDataAdapter("SELECT * from Employees", con);
+            DataTable dt=new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                return JsonConvert.SerializeObject(dt);
+            }
+            else
+            {
+                return "no data found";
+            }
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
